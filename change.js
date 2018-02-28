27baabbc0589f0d6772fcdb48d7d3a2c5c22d87e
Jane <Bo4koJane@i.ua>
2018-02-28 14:16:12 +0200

    Add change.js

---
+++ change.js
@@ -0,0 +1,14 @@
+// Поменять значения переменных местами
+// var a = 17, b = 74;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(a,b);
+
+
+var a = 17, b = 85;
+var c = a;
+a = b;
+b = c;
+console.log(a,b);
+
